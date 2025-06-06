b183956fc9c9cb5883daab75c8da93cbe9b8ff3c
demmonico <10497315+demmonico@users.noreply.github.com>
2025-06-06 15:09:21 +0200

    improve default Lambda code, make it work as a backend for API Gateway. TRIGGER #patch

--- lambda-js-code-template/index.js
+++ lambda-js-code-template/index.js
@@ -1,6 +1,15 @@
 // Just empty initial lambda function for created resources
-exports.handler = (event, ctx) => {
-    const msg = 'Hello, world!';
+exports.handler = async (event, ctx) => {
+    const msg = 'Hello from the dummy lambda!';
     console.log(msg, event, ctx);
-    return msg;
+
+    const respBody = { message: msg }
+    const { version } = event;
+    const resp = version === '2.0' ? { ...respBody } : {
+        statusCode: 200,
+        body: JSON.stringify(respBody)
+    };
+    console.log(version, respBody, resp);
+
+    return resp;
 }
